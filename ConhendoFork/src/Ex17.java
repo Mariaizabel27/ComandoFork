67b42f65fe5ee54aca82386e2e319e87dbd52c1e
camilamorais23 <0001088099@senaimgaluno.com.br>
2024-10-24 15:17:13 -0300

    Adicionando exercico 17

--- ConhendoFork/src/Ex17.java
+++ ConhendoFork/src/Ex17.java
@@ -1,10 +1,25 @@
+
 /*Números de 1 a N: Peça um número N ao usuário e imprima todos os números de 1 até N, 
  * informando se cada número é par ou ímpar.
  */
+/*Desenvolvido por:Maria Izabel e Camila Morais*/
+import java.util.Scanner;
 public class Ex17 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Digite um número N");
+		int N = scanner.nextInt();
+
+		if (N % 2 == 0) {
+			System.out.println("O número digitado é par");
+		}
+
+		else {
+			System.out.println(" O número digitado é ímpar");
+		}
+
+		scanner.close();
 
 	}
 
